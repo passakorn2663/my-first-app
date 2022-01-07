fa7ef959481d1c1e335f738ce8a7f87a619a1bb5
passakorn2663 <passakornbuakeaw@gmail.com>
2022-01-08 03:21:37 +0700

    Finish 3SB03

---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <q-card class="my-card">
+    <q-card-section>
+      Hello world!
+    </q-card-section>
+    </q-card>
+    <q-btn style="top:5px" color="primary" icon="home" label="Go Home" @click="$router.replace('/')"/>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
